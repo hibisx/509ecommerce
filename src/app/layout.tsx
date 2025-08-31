86aab9f9aaeaae11e0772283275341755e8553b3
hibisx <hekuu@icloud.com>
2025-08-31 18:48:24 +0800

    initial styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Jost } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: '509 E-Commerce',
-  description: 'Nike products e-commerce store',
-}
+  title: "Nike E-Commerce",
+  description: "Nike products e-commerce store",
+};
+
+const jost = Jost({ variable: "--font-jost", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        {children}
-      </body>
+      <body className={`${jost.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
